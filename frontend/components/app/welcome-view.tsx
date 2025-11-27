955c9a9b86bf367d7481b65579e3413057d0d6fe
piyushprashantkopte <piyushkopte23@gmail.com>
2025-11-27 09:48:07 +0530

    day6 task comleted

--- frontend/components/app/welcome-view.tsx
+++ frontend/components/app/welcome-view.tsx
@@ -35,7 +35,7 @@ export const WelcomeView = ({
       <section className="bg-background flex flex-col items-center justify-center text-center">
         {/* ⭐ Your new heading */}
         <h1 className="text-4xl font-bold mb-4 text-foreground">
-          SDR Agent
+          Fraud Alert Voice Agent
         </h1>
         <WelcomeImage />
 
